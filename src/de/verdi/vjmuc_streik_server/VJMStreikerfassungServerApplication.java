76df2758511923498bfe14978442876cd1a4868d
Kenneth Böhmer <Krixjun@hotmail.de>
2020-07-06 00:14:23 +0200

    init commit

---
+++ src/de/verdi/vjmuc_streik_server/VJMStreikerfassungServerApplication.java
@@ -0,0 +1,13 @@
+package de.verdi.vjmuc_streik_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VJMStreikerfassungServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VJMStreikerfassungServerApplication.class, args);
+	}
+
+}
---
+++ src/de/verdi/vjmuc_streik_server/ws/RestService.java
@@ -0,0 +1,10 @@
+package de.verdi.vjmuc_streik_server.ws;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestService {
+	
+	// @PutMapping
+	// public
+}
